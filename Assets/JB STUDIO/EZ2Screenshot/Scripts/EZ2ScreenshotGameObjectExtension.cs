3f29e95935f2a48eb71faf34746abffde8783ff6
Ju Hwijung <gnlwnd5084@gmail.com>
2022-05-04 09:17:19 +0900

    Add own GetOrAdd method. Remove unnecessary using directives.

---
+++ Assets/JB STUDIO/EZ2Screenshot/Scripts/EZ2ScreenshotGameObjectExtension.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public static class EZ2ScreenshotGameObjectExtension
+{
+    public static T GetOrAddComponent<T>(this GameObject go) where T : Component
+    {
+        T component = go.GetComponent<T>();
+        
+        if (component == null)
+            component = go.AddComponent<T>();
+        
+        return component;
+    }
+}
--- Assets/JB STUDIO/EZ2Screenshot/Scripts/EZ2ScreenshotObject.cs
+++ Assets/JB STUDIO/EZ2Screenshot/Scripts/EZ2ScreenshotObject.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Camera))]
 public class EZ2ScreenshotObject : MonoBehaviour
