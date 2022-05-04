fb31c235629f489175388643b952800c60339dca
Ju Hwijung <gnlwnd5084@gmail.com>
2022-05-04 09:17:19 +0900

    Add own GetOrAdd method. Remove unnecessary using directives.

--- Assets/JB STUDIO/EZ2Screenshot/Editor/EZ2ScreenshotWindow.cs
+++ Assets/JB STUDIO/EZ2Screenshot/Editor/EZ2ScreenshotWindow.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Web.WebPages;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
