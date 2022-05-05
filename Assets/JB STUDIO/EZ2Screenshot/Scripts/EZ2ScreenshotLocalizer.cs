ee5bc71c95cca037f5d7b88cb1aee527bdfbbab0
Ju Hwijung <gnlwnd5084@gmail.com>
2022-05-06 02:02:53 +0900

    Fix error when build the project

--- Assets/JB STUDIO/EZ2Screenshot/Scripts/EZ2ScreenshotLocalizer.cs
+++ Assets/JB STUDIO/EZ2Screenshot/Scripts/EZ2ScreenshotLocalizer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -62,3 +63,4 @@ public static class EZ2ScreenshotLocalizer
         _data = JsonSerializer.Deserialize<Dictionary<string, string>>(file);
     }
 }
+#endif
--- Assets/JB STUDIO/EZ2Screenshot/Scripts/EZ2ScreenshotObject.cs
+++ Assets/JB STUDIO/EZ2Screenshot/Scripts/EZ2ScreenshotObject.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -147,3 +148,4 @@ public class EZ2ScreenshotObject : MonoBehaviour
         return EZ2ScreenshotLocalizer.TranslateText(key);
     }
 }
+#endif
